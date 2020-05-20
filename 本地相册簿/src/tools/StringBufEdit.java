33aa8269fefe8cba66a696e5e3ec15bf8eb12c39
hlz2516 <714251494@qq.com>
2020-05-20 14:52:41 +0800

    delete src

--- 本地相册簿/src/tools/StringBufEdit.java
+++
@@ -1,8 +0,0 @@
-package tools;
-
-public class StringBufEdit {
-	public static void deleteStringFromTail(StringBuilder string,String subString) {
-		int index = string.lastIndexOf(subString);
-		string.delete(index, string.length());
-	}
-}
